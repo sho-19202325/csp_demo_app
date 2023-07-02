437a9b227a25736ec82cffdab37fba3a568ae7dd
Shota Kuchiki <zk5zg3az@i.softbank.jp>
2023-07-02 18:53:08 +0900

    Add form to create post

--- client/src/pages/posts/index.tsx
+++ client/src/pages/posts/index.tsx
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { CSSProperties, ChangeEvent, FormEvent, useEffect, useState } from 'react'
 import { Post } from 'src/type'
 import axios from 'axios'
 
+const API_HOST = "http://localhost:3001/api/v1"
+
 const IndexPosts = () => {
   const [posts, setPosts] = useState<Post[]>([])
+  const [newPost, setNewPost] = useState<Omit<Post, "id">>({ title: "", content: ""})
 
   const fetchPosts = async () => {
     try {
-      const response = await axios.get<Post[]>("http://localhost:3001/api/v1/posts")
+      const response = await axios.get<Post[]>(`${API_HOST}/posts`)
       setPosts(response.data)
     } catch(error) {
       console.error(error)
+    }
+  }
+
+  const handleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNewPost({...newPost, title: event.target.value})
+  }
+
+  const handleContentChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    setNewPost({...newPost, content: event.target.value})
+  }
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post<Post>(`${API_HOST}/posts`, newPost)
+      setPosts([...posts, response.data])
+      setNewPost({ title: "", content: "" })
+    } catch (error) {
+      console.error(error);
     }
   }
 
@@ -18,19 +41,33 @@ const IndexPosts = () => {
     fetchPosts()
   }, [])
 
-  return <>
-    <h1>新規作成</h1>
-    {/* TODO: 投稿フォーム作成 */}
+  return (
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center"
+    }}>
+      <h1>新規作成</h1>
+      <form onSubmit={handleSubmit} style={{
+            width: 500,
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px"
+      }}>
+        <input type="text" value={newPost.title} onChange={handleTitleChange} maxLength={50} placeholder='title' required />
+        <textarea value={newPost.content} onChange={handleContentChange} maxLength={1000} placeholder='content' />
+        <button type="submit">登録</button>
+      </form>
 
-    <h1>投稿一覧</h1>
-    {posts.map((post) => (
-      <div key={post.id}>
-        <h2>{post.title}</h2>
-        <p>{post.content}</p>
-      </div>
-    ))}
-    
-  </>
+      <h1>投稿一覧</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default IndexPosts
